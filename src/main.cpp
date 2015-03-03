f6d6f000724a4cb21561e80414aea5a1e9bf5dd6
gartenriese <gartenriese@gulli.com>
2015-03-03 23:40:05 +0100

    imgui test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <cstdlib>
 
-#include "demo.hpp"
+//#include "demo.hpp"
+#include "engine/engine.hpp"
+#include "engine/gl/buffer.hpp"
+#include "engine/extern/imgui.h"
 
 int main() {
 
-	Demo demo({1920, 1080});
-	while (demo.render());
+	//Demo demo({800, 600});
+	//while (demo.render());
+
+	engine::Engine e({800, 600}, "Test", true);
+
+	e.getInputPtr()->addKeyFunc([&](const int key, const int, const int action, const int){
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+			glfwSetWindowShouldClose(e.getWindowPtr()->getGLFWWindow(), GL_TRUE);
+		}
+	});
+
+	glClearColor(0,0,0,0);
+
+	while (e.render()) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		e.getGuiPtr()->update();
+		ImGui::Text("Hallo Welt");
+		e.getGuiPtr()->render();
+	}
 
 	return EXIT_SUCCESS;
 
